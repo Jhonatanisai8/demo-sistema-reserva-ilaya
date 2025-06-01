2c9e704a061878b7b6422a3c84c0870374c887a2
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-01 13:30:32 -0500

    init

---
+++ src/main/java/com/isai/demosistemacitasillaya/app/DemoSistemaCitasIllayaApplication.java
@@ -0,0 +1,13 @@
+package com.isai.demosistemacitasillaya.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSistemaCitasIllayaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSistemaCitasIllayaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isai/demosistemacitasillaya/app/DemoSistemaCitasIllayaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.demosistemacitasillaya.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSistemaCitasIllayaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
