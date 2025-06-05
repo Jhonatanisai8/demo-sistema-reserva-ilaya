373ecae742c1ce3ac1b8703e869917df7af1cf23
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-05 17:06:15 -0500

    anums para las reservas

--- src/main/java/com/isai/demosistemacitasillaya/app/models/emuns/EstadoReserva.java
+++ src/main/java/com/isai/demosistemacitasillaya/app/models/emuns/EstadoReserva.java
@@ -1,8 +1,19 @@
 package com.isai.demosistemacitasillaya.app.models.emuns;
 
 public enum EstadoReserva {
-    PENDIENTE,
-    CONFIRMADA,
-    CANCELADA,
-    ASISTIO
+    PENDIENTE("Pendiente"),
+    CONFIRMADA("Confirmada"),
+    CANCELADA("Cancelada"),
+    ASISTIO("Asistio");
+
+    private final String descripcion;
+
+    EstadoReserva(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
 }
