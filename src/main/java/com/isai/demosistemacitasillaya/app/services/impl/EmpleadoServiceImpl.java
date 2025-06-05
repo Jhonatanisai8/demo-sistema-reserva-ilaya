81166463047b2d43ab2f017fbb78862ddfaedd7b
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-05 01:31:22 -0500

    implementamos los metodos crud de la interface service de empleado

---
+++ src/main/java/com/isai/demosistemacitasillaya/app/services/impl/EmpleadoServiceImpl.java
@@ -0,0 +1,47 @@
+package com.isai.demosistemacitasillaya.app.services.impl;
+
+import com.isai.demosistemacitasillaya.app.models.Empleado;
+import com.isai.demosistemacitasillaya.app.repositorys.EmpleadoRepository;
+import com.isai.demosistemacitasillaya.app.services.EmpleadoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class EmpleadoServiceImpl implements EmpleadoService {
+
+    private final EmpleadoRepository empleadoRepository;
+
+    @Override
+    @Transactional
+    public List<Empleado> findAllEmpleados() {
+        return empleadoRepository.findAll();
+    }
+
+    @Override
+    @Transactional
+    public List<Empleado> searchEmpleados(String terminoBusqueda) {
+        return empleadoRepository.searchEmpleados(terminoBusqueda);
+    }
+
+    @Override
+    @Transactional
+    public Optional<Empleado> findEmpleadoById(Integer idBuscado) {
+        return empleadoRepository.findById(idBuscado);
+    }
+
+    @Override
+    @Transactional
+    public Empleado saveEmpleado(Empleado cliente) {
+        return null;
+    }
+
+    @Override
+    public void deleteEmpleadoById(Integer idBuscado) {
+
+    }
+}
