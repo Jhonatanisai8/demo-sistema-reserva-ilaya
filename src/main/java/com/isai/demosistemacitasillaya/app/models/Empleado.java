659dd11142397d1038da8ce1e5d94e131860fd78
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-05 16:45:31 -0500

    agregacion de validaciones en la clase empleado

--- src/main/java/com/isai/demosistemacitasillaya/app/models/Empleado.java
+++ src/main/java/com/isai/demosistemacitasillaya/app/models/Empleado.java
@@ -2,10 +2,15 @@ package com.isai.demosistemacitasillaya.app.models;
 
 import com.isai.demosistemacitasillaya.app.models.emuns.EstadoEmpleado;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -22,34 +27,47 @@ public class Empleado {
     @Column(name = "id_empleado")
     private Integer idEmpleado;
 
+    @NotNull
+    @Size(min = 3, max = 50)
     @Column(name = "nombres", nullable = false, length = 50)
     private String nombres;
 
+    @NotNull
+    @Size(min = 3, max = 50)
     @Column(name = "apellidos", nullable = false, length = 50)
     private String apellidos;
 
-    @Column(name = "dni", unique = true, length = 15)
+    @NotNull
+    @Size(max = 8)
+    @Column(name = "dni", unique = true, length = 8)
     private String dni;
 
     @Column(name = "fecha_nacimiento")
     private LocalDate fechaNacimiento;
 
-    @Column(name = "telefono", length = 15)
+    @NotEmpty
+    @Size(max = 9)
+    @Column(name = "telefono", length = 9)
     private String telefono;
 
     @Column(name = "email", unique = true, length = 100)
     private String email;
 
+    @NotNull
+    @Size(max = 50)
     @Column(name = "cargo", nullable = false, length = 50)
     private String cargo;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Column(name = "fecha_contratacion", columnDefinition = "DATE DEFAULT (CURRENT_DATE)")
     private LocalDate fechaContratacion;
 
+    @NotBlank
     @Column(name = "salario", precision = 10, scale = 2)
     private BigDecimal salario;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "estado", nullable = false)
+    @NotNull
     private EstadoEmpleado estado;
 }
