0e1301b052c2cfd19a0ce82856292d22379af057
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-05 01:05:05 -0500

    interface con la definicion de metodos para la entidad lugar

--- src/main/java/com/isai/demosistemacitasillaya/app/services/LugarService.java
+++ src/main/java/com/isai/demosistemacitasillaya/app/services/LugarService.java
@@ -3,7 +3,13 @@ package com.isai.demosistemacitasillaya.app.services;
 import com.isai.demosistemacitasillaya.app.models.Lugar;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LugarService {
+
     List<Lugar> searchLugares(String terminoBusqueda);
+
+    List<Lugar> findAllLugares();
+
+    Optional<Lugar> findLugarById(Integer id);
 }
