6f8a88c9b8b9dfacc01622747177b90499fa85ed
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-05 01:30:58 -0500

    creacion de un metodo para buscar un empleado por su nombres o apellidos

--- src/main/java/com/isai/demosistemacitasillaya/app/repositorys/EmpleadoRepository.java
+++ src/main/java/com/isai/demosistemacitasillaya/app/repositorys/EmpleadoRepository.java
@@ -1,10 +1,20 @@
 package com.isai.demosistemacitasillaya.app.repositorys;
 
 import com.isai.demosistemacitasillaya.app.models.Empleado;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmpleadoRepository
-        extends CrudRepository<Empleado, Integer> {
+        extends JpaRepository<Empleado, Integer> {
+
+    @Query("SELECT e FROM Empleado e WHERE LOWER(e.nombres)" +
+            " LIKE LOWER(CONCAT('%', :terminoBusqueda, '%')) " +
+            "OR LOWER(e.apellidos) LIKE LOWER(CONCAT('%', :terminoBusqueda, '%'))")
+    List<Empleado> searchEmpleados(@Param("terminoBusqueda") String terminoBusqueda);
+
 }
