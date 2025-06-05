6fd46b78dbe993cb4aa8b2589b979a0ad926cc12
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-05 00:18:51 -0500

    funcinalidad para ver los detalles de un cliente seleccionado

--- src/main/java/com/isai/demosistemacitasillaya/app/controllers/admin/AdminClienteController.java
+++ src/main/java/com/isai/demosistemacitasillaya/app/controllers/admin/AdminClienteController.java
@@ -3,11 +3,14 @@ package com.isai.demosistemacitasillaya.app.controllers.admin;
 import com.isai.demosistemacitasillaya.app.models.Cliente;
 import com.isai.demosistemacitasillaya.app.services.impl.ClienteServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -30,5 +33,14 @@ public class AdminClienteController {
         model.addAttribute("clientesBD", clientes);
         model.addAttribute("terminoBusqueda", terminoBusqueda);
         return "admin/clientes/list";
+    }
+
+    @GetMapping("/{id}")
+    public String verDetallesCliente(@PathVariable("id") Integer id, Model model) {
+        Cliente cliente = clienteService.findClienteById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente no encontrado con ID: " + id));
+
+        model.addAttribute("cliente", cliente);
+        return "admin/clientes/detail";
     }
 }
